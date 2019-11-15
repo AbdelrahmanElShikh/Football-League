a9522c1e304c172621f489fa7c6acd7560840117
Abdel-Rahman <a.rahmanelshikh@gmail.com>
2019-11-15 20:59:38 +0200

    adding MyApplication class

--- app/src/main/java/com/abdelrahman/footballleague/MyApplication.java
+++ app/src/main/java/com/abdelrahman/footballleague/MyApplication.java
@@ -1,10 +1,36 @@
 package com.abdelrahman.footballleague;
 
 import android.app.Application;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 
 /**
  * @author Abdel-Rahman El-Shikh on 15-Nov-19.
+ * This class is responsible for the network checking function of the app.
  */
 public class MyApplication extends Application {
+    public static MyApplication instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if(instance == null)
+            instance = this;
+    }
+
+    public static MyApplication getInstance(){
+        return instance;
+    }
+    public static boolean hasNetwork(){
+        return instance.isNetworkAvailable();
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager cm =(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        assert cm != null;
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
 }
