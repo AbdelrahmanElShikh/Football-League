23db6822592253b21f2358a48407e581c24d199c
Abdel-Rahman <a.rahmanelshikh@gmail.com>
2019-11-15 20:42:23 +0200

    initial commit

---
+++ app/src/main/java/com/abdelrahman/footballleague/MyApplication.java
@@ -0,0 +1,10 @@
+package com.abdelrahman.footballleague;
+
+import android.app.Application;
+
+
+/**
+ * @author Abdel-Rahman El-Shikh on 15-Nov-19.
+ */
+public class MyApplication extends Application {
+}
